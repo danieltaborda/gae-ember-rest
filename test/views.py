5b5171e284c39d6cec873f2bd837ed9d330ee0bf
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-08-22 23:12:47 +0300

    refactor

--- test/views.py
+++ test/views.py
@@ -1,6 +1,12 @@
 import os
+
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+os.environ['DJANGO_SETTINGS_MODULE'] = '__init__'
+
 import webapp2 as webapp
 from google.appengine.ext.webapp import template
+
 import apis
 
 
